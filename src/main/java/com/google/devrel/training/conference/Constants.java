5903b637993df9c1a920605146aacb1dd277a88f
jgraniero <joseph.graniero@nrelate.com>
2014-10-05 13:27:37 -0400

    Updated project so that it actually runs

--- src/main/java/com/google/devrel/training/conference/Constants.java
+++ src/main/java/com/google/devrel/training/conference/Constants.java
@@ -6,7 +6,9 @@ import com.google.api.server.spi.Constant;
  * Contains the client IDs and scopes for allowed clients consuming the conference API.
  */
 public class Constants {
-    public static final String WEB_CLIENT_ID = "replace this with your web client id";
+    public static final String WEB_CLIENT_ID = 
+    	"411168634845-mlu6v76b6j5uevnqgvjoot8t7527u4j6.apps.googleusercontent.com";
+
     public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
     public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'your-client-id',
+        CLIENT_ID: '411168634845-mlu6v76b6j5uevnqgvjoot8t7527u4j6.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
